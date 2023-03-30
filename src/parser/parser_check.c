c2d216820ac0a5b307d8a049f5c56e0d5e2789d8
aquincho <aquinchon.test1@gmail.com>
2023-03-30 12:02:27 +0200

    norminette 2

--- src/parser/parser_check.c
+++ src/parser/parser_check.c
@@ -6,7 +6,7 @@
 /*   By: aquincho <aquincho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 10:50:43 by aquincho          #+#    #+#             */
-/*   Updated: 2023/03/30 11:25:53 by aquincho         ###   ########.fr       */
+/*   Updated: 2023/03/30 12:00:09 by aquincho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,56 +34,63 @@ static void	ft_start_pos(t_data *data, int i, int j)
 	}
 }
 
+static int	ft_check_line(char **map, t_data *data, int i, int *nb_perso)
+{
+	int	j;
+
+	j = 0;
+	while (j < (int)ft_strlen((map[i])))
+	{
+		while (j < (int)ft_strlen((map[i])) && map[i][j] == ' ')
+		{
+			if ((i > 0
+				&& !(map[i - 1][j] == ' ' || map[i - 1][j] == '1'))
+				|| (i < data->height - 1
+				&& !(map[i + 1][j] == ' ' || map[i + 1][j] == '1'))
+				|| (map[i][j - 1]
+				&& !(map[i][j - 1] == ' ' || map[i][j - 1] == '1'))
+				|| (map[i][j + 1]
+				&& !(map[i][j + 1] == ' ' || map[i][j + 1] == '1')))
+				return (EXIT_FAILURE);
+			j++;
+		}
+		if ((i == 0 || i == data->height - 1
+			|| j == 0 || j == (int)ft_strlen(map[i]) - 1)
+			&& !(map[i][j] == '1' || map[i][j] == ' '))
+			return (EXIT_FAILURE);
+		if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'W'
+			|| map[i][j] == 'E')
+		{
+			ft_start_pos(data, i, j);
+			if (nb_perso < 1)
+				nb_perso++;
+			else
+				return (EXIT_FAILURE);
+		}
+		if (map[i][j] == '0' && (map[i - 1][j] == ' ' || map[i + 1][j] == ' '
+		|| map[i][j - 1] == ' ' || map[i][j + 1] == ' '))
+			return (EXIT_FAILURE);
+		if (map[i][j] == '0' && ((i > 0
+			&& j > (int)ft_strlen(map[i - 1]))
+			|| (i < data->height - 1 && j > (int)ft_strlen(map[i - 1]))))
+			return (EXIT_FAILURE);
+		j++;
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	ft_check_map(char **map, t_data *data)
 {
 	int	i;
-	int	j;
 	int	nb_perso;
 
 	nb_perso = 0;
 	i = 0;
 	while (map[i])
 	{
-		j = 0;
-		while (j < (int)ft_strlen((map[i])))
-		{
-			while (j < (int)ft_strlen((map[i])) && map[i][j] == ' ')
-			{
-				if ((i > 0
-					&& !(map[i - 1][j] == ' ' || map[i - 1][j] == '1'))
-					|| (i < data->height - 1
-					&& !(map[i + 1][j] == ' ' || map[i + 1][j] == '1'))
-					|| (map[i][j - 1]
-					&& !(map[i][j - 1] == ' ' || map[i][j - 1] == '1'))
-					|| (map[i][j + 1]
-					&& !(map[i][j + 1] == ' ' || map[i][j + 1] == '1')))
-					return (EXIT_FAILURE);
-				j++;
-			}
-			if ((i == 0 || i == data->height - 1
-				|| j == 0 || j == (int)ft_strlen(map[i]) - 1)
-				&& !(map[i][j] == '1' || map[i][j] == ' '))
-				return (EXIT_FAILURE);
-			if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'W'
-				|| map[i][j] == 'E')
-			{
-				ft_start_pos(data, i, j);
-				if (nb_perso < 1)
-					nb_perso++;
-				else
-					return (EXIT_FAILURE);
-			}
-			if (map[i][j] == '0' && (map[i - 1][j] == ' ' || map[i + 1][j] == ' '
-			|| map[i][j - 1] == ' ' || map[i][j + 1] == ' '))
-				return (EXIT_FAILURE);
-			if (map[i][j] == '0' && ((i > 0
-				&& j > (int)ft_strlen(map[i - 1]))
-				|| (i < data->height - 1 && j > (int)ft_strlen(map[i - 1]))))
-				return (EXIT_FAILURE);
-			j++;
-		}
+		if (ft_check_line(map, data, i, &nb_perso))
+			return (EXIT_FAILURE);
 		i++;
 	}
-	(void)data;
 	return (EXIT_SUCCESS);
 }
